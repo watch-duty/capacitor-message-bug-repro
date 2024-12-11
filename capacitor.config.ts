af1ec0dbd30de5f11a5f481b8a253552a69420c2
gabeschine <gabe@schine.net>
2024-12-11 16:21:16 -0500

    cap add ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.watchduty.capmessagebugrepro',
+  appName: 'capacitor-message-bug-repro',
+  webDir: 'dist'
+};
+
+export default config;
