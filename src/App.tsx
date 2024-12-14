a12d6cbfff8eb821fc4c0b62dd3ba84bc8b62f19
gabeschine <gabe@schine.net>
2024-12-14 15:41:11 -0500

    show last urlopen

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { App as CapacitorApp } from "@capacitor/app";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [lastUrl, setLastUrl] = useState("");
+  const [lastUrlDate, setLastUrlDate] = useState<Date | null>(null);
+
+  useEffect(() => {
+    CapacitorApp.addListener("appUrlOpen", (event) => {
+      alert("URL opened: " + event.url);
+      setLastUrl(event.url);
+      setLastUrlDate(new Date());
+      setCounter((prev) => prev + 1);
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Last URL: {lastUrl}</p>
+        <p>Last event date: {lastUrlDate?.toString()}</p>
+        <p>Count: {counter}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
