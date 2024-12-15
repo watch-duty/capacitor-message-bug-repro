0feba19101a5b5deadf4eac455bc9d6e4c456d08
gabeschine <gabe@schine.net>
2024-12-15 13:30:35 -0500

    save intermediate outputs

--- go.sh
+++ go.sh
@@ -3,6 +3,11 @@
 # set -e
 
 TARGET=$1
+OUTPUT_DIR=${2:-/tmp}
+ITERATION=${3:-1}
+
+PNG_PATH=$OUTPUT_DIR/$ITERATION.png
+TXT_PATH=$OUTPUT_DIR/$ITERATION.txt
 
 rm -f /tmp/capmessagebug.*
 
@@ -12,14 +17,14 @@ sh ./try_repro.sh $TARGET
 sleep 2
 
 # take a screenshot
-xcrun simctl io $TARGET screenshot /tmp/capmessagebug.png
+xcrun simctl io $TARGET screenshot $PNG_PATH
 
 # run ocr on it
 set -e
-shortcuts run "Grab Text from Image" --input-path /tmp/capmessagebug.png --output-path /tmp/capmessagebug.txt
+shortcuts run "Grab Text from Image" --input-path $PNG_PATH --output-path $TXT_PATH
 set +e
 
-if grep "YES IT WORKED" /tmp/capmessagebug.txt; then
+if grep "YES IT WORKED" $TXT_PATH; then
   exit 0
 else
   exit 1
--- go_loop.sh
+++ go_loop.sh
@@ -9,15 +9,23 @@ COUNT=$2
 
 RESULTS_ARRAY=()
 
+OUTPUT_DIR="$(pwd)/output/$(date -Iminutes)"
+echo "Output dir: $OUTPUT_DIR"
+echo
+
+mkdir -p $OUTPUT_DIR
+cp package.json capacitor.config.ts $OUTPUT_DIR
+
 for i in $(seq 1 $COUNT); do
   echo "Iteration $i"
-  if sh ./go.sh $TARGET; then
+  if sh ./go.sh $TARGET $OUTPUT_DIR $i; then
     RESULTS_ARRAY+=(".")
   else
     RESULTS_ARRAY+=("F")
   fi
+  
   echo
 done
 
-echo -n "Results: "
-echo "${RESULTS_ARRAY[@]}" | sed 's/ //g'
+echo "${RESULTS_ARRAY[@]}" | sed 's/ //g' > $OUTPUT_DIR/summary.txt
+cat $OUTPUT_DIR/summary.txt
